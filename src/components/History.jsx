77b316840bdf4015c776f92f678d631f48513e72
Abdimatin Abdullahi <abdimatmicrosoft@gmail.com>
2025-09-12 19:30:59 +0300

    History UI structured

--- src/components/History.jsx
+++ src/components/History.jsx
@@ -1,8 +1,26 @@
-import React from 'react'
+import React, { useState } from 'react'
+import style from '../styles/components/history.module.css'
+
 
 function History() {
+
+  const [ historyMeetings, sethistoryMeetings] = useState([])
+
   return (
-    <div>History</div>
+    <div className={style.historyContainer}>
+        <h3> Past meetings </h3>
+        {
+          historyMeetings.length > 0 ? historyMeetings.map((meeting)=>(
+            <div className={style.meeting}> 
+              
+            </div>
+          )) : (
+            <div className={style.noMeeting}>
+                No meeting happened
+            </div>
+          )
+        }
+    </div>
   )
 }
 
