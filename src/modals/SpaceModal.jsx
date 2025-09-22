b4d9459deab10b7428395916070677c33b565f5e
Abdimatin Abdullahi <abdimatmicrosoft@gmail.com>
2025-09-22 15:19:06 +0300

    Space Modal started

--- src/modals/SpaceModal.jsx
+++ src/modals/SpaceModal.jsx
@@ -1,8 +1,11 @@
-import React from 'react'
+import React, { useState } from 'react'
 import style from '../styles/modals/spacemodal.module.css'
-import { X } from 'lucide-react'
+import { Activity, CircleOff, Hourglass, Settings, Trash, X } from 'lucide-react'
 
 function SpaceModal({ onClose }) {
+
+  const [Members, setMembers] = useState([])
+  const [invitations, setInvitations] = useState([])
 
   return (
     <div className={style.overLay} >
@@ -10,13 +13,106 @@ function SpaceModal({ onClose }) {
 
             <div className={style.headerBar}> 
               
-              <h2> Space </h2>
+              <div className={style.spaceSetings}>
+
+                  <div className={style.spaceIcon} >
+                    <Activity size={30} strokeWidth={2.5} />
+                  </div>
               
-              <div>spaceone.com</div>
+                  <div className={style.spaceName} >
+                    Space One
+                  </div>
 
-              <div onClick={onClose} > Close </div>
+                  <div className={style.settings} > 
+                    Settings
+                  </div>
 
-            </div>        
+              </div>
+
+              <div onClick={onClose} className={style.close} > Close </div>
+
+            </div> 
+
+
+            <div className={style.timeZone}>
+                <div className={style.icon}>
+                  <Hourglass/>
+                </div>
+
+                <div className={style.section2}>
+                    <h3>Time Zone</h3>
+                    <span> The current time zone is: Nairobi GMT +3 </span>
+                </div>
+
+              <select>
+                <option value=""> Select Time zone </option>
+                <option value=""> Nairobi GMT +3 </option>
+                <option value=""> Beiien GMT +12 </option>
+                <option value=""> USA GMT -12 </option>
+                <option value=""> Riyadh -3 </option>
+              </select>
+
+            </div>
+
+
+            <div className={style.spaceMembers} >
+              <h3> Space Members </h3>
+
+              { Members.length > 0 ? Members.map((member)=>(
+                  <div className={style.memberContainer} >
+                      <div className={style.memberSecOne} >
+                          <h3> Ramla Hussien </h3>
+                          <span> Joined at 30 sep 2025 </span>
+                      </div>
+
+                      <div className={style.memberSecTwo}>
+                        Admin
+                      </div>
+
+                      <div className={style.iconRemove}>
+                        <Trash/>
+                      </div>
+
+                  </div>
+              )): (
+                <div className={style.noMember} >
+                      <CircleOff size={50} style={{ color: "#9900cc" }}  />
+                      <span> No Members yet </span>
+                </div>
+              )}
+
+            </div>  
+
+            
+
+            <div className={style.invitations} >
+              <h3> Invitations </h3>
+
+                { invitations.length > 0 ? invitations.map((invite)=>(
+                  <div className={style.memberContainer} >
+                      <div className={style.memberSecOne} >
+                          <h3> Ramla Hussien </h3>
+                          <span> Joined at 30 sep 2025 </span>
+                      </div>
+
+                      <div className={style.memberSecTwo}>
+                        Admin
+                      </div>
+
+                      <div className={style.iconRemove}>
+                        <Trash/>
+                      </div>
+
+                  </div>
+                )) : (
+                  <div className={style.noMember} >
+                    <CircleOff size={50} style={{ color: "#9900cc" }} />
+                    No Pending Invitations
+                  </div>
+                ) }
+
+              <button> Invite to space </button>
+            </div>      
         
         </div>
     </div>
