77f183e067bdc3274fc5c52f5731a7e35c7a1209
Abdimatin Abdullahi <abdimatmicrosoft@gmail.com>
2025-09-27 10:34:19 +0300

    Date solved

--- src/modals/MeetingModal.jsx
+++ src/modals/MeetingModal.jsx
@@ -1,7 +1,8 @@
 import React, { useState } from "react";
 import style from "../styles/modals/meeting.module.css";
 import { times, tomorrowsDate } from '../utils/times'
-
+import Spinner from '../components/Spinner'
+import Toastify from '../modals/Toastify'
 import {
   CalendarClock,
   Clock,
@@ -11,11 +12,68 @@ import {
 
 function MeetingModal({ onClose }) {
 
-  const [meetingName, setMeetingName] = useState("")
-  const startTime = tomorrowsDate("Africa/Nairobi")
-  console.log("Tomorrows date: ", startTime)
 
-  const [statTime, setstatTime] = useState(startTime)
+  // getting tomorows date from utils function
+
+  const startTime = tomorrowsDate("Africa/Nairobi")
+  // getting tomorows date for space timezone
+  const [ meetingName, setMeetingName ] = useState("")
+  const [ startDate, setStartDate ] = useState(startTime)
+  const [ selectedHour, setSelectedHour ] = useState(times[0].value)
+  const [ whoCanPresent, setWhoCanPresent ] = useState("me")
+  
+  const [ loading, setLoading ] = useState(false)
+  const [ error, setError ] = useState("")
+  const [ duration, setDuration] = useState("")
+
+
+  const handleCreateMeeting = async ()=>{
+      setError("");
+      if (!meetingName.trim()) {
+        setError("Provide meeting name");
+        return;
+      }
+      if (!whoCanPresent) {
+        setError("Select who can present");
+        return;
+      }
+      if (!duration) {
+        setError("Enter meeting duration");
+        return;
+      }
+
+      const dur = Number(duration);
+      if (isNaN(dur) || dur <= 0) {
+        setError("Duration must be a positive number");
+        return;
+      }
+      if (dur >= 24) {
+        setError("You can't meet for more than 24 hours");
+        return;
+      }
+
+      setLoading(true)
+      const durationHours = parseFloat(dur)
+
+      const meetingStartTime = new Date(`${startDate}T${selectedHour}`)
+      const localDate = new Date(meetingStartTime.toISOString())
+      const endUTC = new Date(localDate.getTime() + durationHours * 60 * 60 * 1000)
+
+      console.log("Meeting start Time", meetingStartTime)
+      console.log("Local date start: ", localDate)
+      console.log("Local date end: ", endUTC)
+
+      const meetingEndUT = endUTC.toISOString()
+      const meetingStart = localDate.toISOString()
+
+      console.log("Equivalent time of the meeting start time in UTC: ", meetingStart)
+      console.log("Equivalent time of the meeting end time in UTC: ", meetingEndUT)
+
+      setTimeout(() => {
+        setLoading(false)
+      }, 500);
+
+  }
 
 
   return (
@@ -41,14 +99,19 @@ function MeetingModal({ onClose }) {
           </div>
         </div>
 
+          { error !== "" && ( <Toastify type="error" onClose={()=>setError("")} message={error} /> ) }
+
+
+        {/* Input name container */}
         <div className={style.inputContainer}>
           <div className={style.edit}>
             <Edit2 className={style.iconTwo} size={30} />
           </div>
-
-          <input type="text" placeholder="Enter the meeting name" />
+          <input type="text" placeholder="Enter the meeting name" value={meetingName} onChange={(e)=>setMeetingName(e.target.value)} />
         </div>
 
+
+        {/* Dates container */}
         <div className={style.dates}>
           <div className={style.clockIcon}>
             <Clock className={style.iconTwo} size={30} />
@@ -56,9 +119,9 @@ function MeetingModal({ onClose }) {
 
           <div className={style.datesContainer}>
 
-            <div className={style.startTime}>
-              <input type="date" value={startTime} min={startTime} />
-              <select className={style.hour} >
+            <div className={style.startTime}  >
+              <input type="date" value={startDate}  onChange={(e)=> setStartDate(e.target.value)} min={startTime} />
+              <select className={style.hour} value={selectedHour} onChange={(e)=>setSelectedHour(e.target.value)}  >
                   { times.map((time)=>(
                     <option value={time.value}> {time.label} </option>
                   )) }
@@ -66,7 +129,7 @@ function MeetingModal({ onClose }) {
             </div>
 
             <div className={style.endTime}>
-              <input type="number" placeholder="Enter the duration in hours" min={0.5} max={24} />
+              <input type="number" placeholder="Enter the duration in hours" min={0.5} max={24} value={duration} onChange={(e)=>setDuration(e.target.value)} />
             </div>
 
           </div>
@@ -74,14 +137,14 @@ function MeetingModal({ onClose }) {
 
         <div className={style.presentContainer}>
           <Presentation />
-          <select>
+          <select value={whoCanPresent} onChange={(e)=>setWhoCanPresent(e.target.value)} >
             <option value="" disabled selected hidden> Who can present? </option>
             <option value="everyone"> Everyone </option>
-            <option value="me only"> Me only </option>
+            <option value="me"> Me only </option>
           </select>
         </div>
 
-        <div className={style.createMeeting}> Create Meeting </div>
+        <div className={style.createMeeting} onClick={handleCreateMeeting} > { loading ? <Spinner/> : " Create Meeting"  } </div>
 
       </div>
     </div>
